a548b24ed4e76909b31b76ca2e47c142d88815c9
sophia-chingyan <wcy.sophia@gmail.com>
2026-03-29 22:00:53 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,5 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
 app.use(cors({ origin: '*', methods: ['GET', 'POST', 'DELETE', 'OPTIONS'] }));
 // ... rest of your code
